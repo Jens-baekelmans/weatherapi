9a3987805945a0a696b131f271c287be482fbd13
Jens-baekelmans <jens.baekelmans@collibra.com>
2023-02-07 16:50:47 +0100

    removed streamlit logo

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 import streamlit as st
 import requests
 
+hide_streamlit_style = """
+            <style>
+            footer {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_streamlit_style, unsafe_allow_html=True)
 
 response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=50.85&longitude=4.35&hourly=temperature_2m")
 st.write(response.json()['hourly']['temperature_2m'])
