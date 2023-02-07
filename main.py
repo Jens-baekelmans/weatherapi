4c57a8dc327835522378c96f8a7c7b9ee14c5f16
Jens-baekelmans <jens.baekelmans@collibra.com>
2023-02-07 16:49:39 +0100

    added streamlit

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
+import streamlit as st
 import requests
 
 
 response = requests.get("https://api.open-meteo.com/v1/forecast?latitude=50.85&longitude=4.35&hourly=temperature_2m")
-print(response.json()['hourly']['temperature_2m'])
+st.write(response.json()['hourly']['temperature_2m'])
 
 
 
